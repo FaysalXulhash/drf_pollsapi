d692f6071e5074554961d229069a47413610592d
FaysalXulhash <faysalahammed80@gmail.com>
2024-11-23 18:30:28 +0600

    cured implement with generics api view

--- poll/urls.py
+++ poll/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
-
+from .views import (
+    PollApi,
+    PollDetailApi,
+    ChoiceApi,
+    VoteApi,
+)
 
 urlpatterns = [
-
+    path('poll-api/', PollApi.as_view(), name='poll_api'),
+    path('choice-api/', ChoiceApi.as_view(), name='choice_api'),
+    path('vote-api/', VoteApi.as_view(), name='vote_api'),
+    path('poll-api/<int:pk>/', PollDetailApi.as_view(), name='poll_detail_api'),
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.generics import (CreateAPIView,
+                                     ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView
+                                     )
+from rest_framework.response import Response
+from .models import Poll, Vote, Choice
+from .serializers import PollSerializer, VoteSerializer, ChoiceSerializer
 # Create your views here.
+
+
+class PollApi(ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+class PollDetailApi(RetrieveUpdateDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+class ChoiceApi(ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+class VoteApi(CreateAPIView):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+
